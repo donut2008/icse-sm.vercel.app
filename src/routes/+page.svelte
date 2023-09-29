0dd268ed936ba16f0c116b943297a97733bf352d
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-09-29 23:07:19 +0530

    Update some code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
 <script>
-    import { Alert } from "flowbite-svelte";
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+	} from "flowbite-svelte";
 </script>
 
-<!-- code goes here -->
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<img
+			src="https://flowbite.com/docs/images/logo.svg"
+			class="mr-3 h-6 sm:h-9"
+			alt="Flowbite Logo"
+		/>
+		<span
+			class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+		>
+			NoteMaster
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/">Question banks</NavLi> <!-- question papers, decide whether to include answer keys or not -->
+		<NavLi href="/">Programs</NavLi> <!-- programs in java, c++, and python -->
+		<NavLi href="/about">About</NavLi>
+	</NavUl>
+	<!-- I'LL WORK ON THIS AFTER I RETURN FROM KOLKATA -->
+</Navbar>
