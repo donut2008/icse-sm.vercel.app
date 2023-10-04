f4338517a590ee80c2dd57e028842720ed03e7ab
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 00:11:33 +0530

    Create NavUl.svelte

---
+++ src/NavUl.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from "$app/stores";
+    import { NavUl } from "flowbite-svelte";
+    $: activeUrl = $page.url.pathname;
+    let activeClass =
+        "text-white bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
+    let nonActiveClass =
+        "text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+</script>
+
+<NavUl {activeUrl} {activeClass} {nonActiveClass}>
+    <slot />
+</NavUl>
