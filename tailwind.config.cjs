549725a70bb94bdb1507430fcc6e8febab0eff76
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 00:11:53 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,22 @@ const config = {
 	darkMode: 'media',
 
 	theme: {
+		fontFamily: {
+			'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+			'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+			'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+
+		},
 		extend: {
+			fontFamily: {
+				'sans': [
+					'"Inter", sans-serif',
+					{
+						fontFeatureSettings: '"cv11", "ss01"',
+						fontVariationSettings: '"opsz" 32'
+					}
+				]
+			},
 			colors: {
 				// flowbite-svelte
 				primary: {
