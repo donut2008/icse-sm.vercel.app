7ec5086b13f2df7b04076a0efa1d1743946f01ae
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 00:11:48 +0530

    Update +page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { page } from "$app/stores";
 	import {
 		Navbar,
 		NavBrand,
@@ -8,6 +9,11 @@
         Heading,
         P,
 	} from "flowbite-svelte";
+    $: activeUrl = $page.url.pathname;
+    let activeClass =
+        "text-white bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
+    let nonActiveClass =
+        "text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
 </script>
 
 <Navbar let:hidden let:toggle>
@@ -24,14 +30,14 @@
 		</span>
 	</NavBrand>
 	<NavHamburger on:click={toggle} />
-	<NavUl {hidden}>
+	<NavUl {activeUrl} {activeClass} {nonActiveClass}>
 		<NavLi href="/home" active={true}>Home</NavLi>
 		<NavLi href="/banks">Question banks</NavLi> <!-- question papers, decide whether to include answer keys or not -->
 		<NavLi href="/programs">Programs</NavLi> <!-- programs in java, c++, and python -->
 		<NavLi href="/about">About</NavLi>
 	</NavUl>
 </Navbar>
-<div class="text-center">
-    <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl">Welcome to ICSE Study Material!</Heading>
-    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">This site is still under construction. Feel free to look around to see what's done (if anything is, so far)!</P>
+<div class="text-center object-center">
+    <Heading tag="h1" class="mt-16 mb-16" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl text-black dark:text-white">Welcome to ICSE Study Material!</Heading>
+    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 text-gray-100 dark:text-gray-400">This site is still under construction. Feel free to look around to see what's done (if anything is, so far)!</P>
 </div>
