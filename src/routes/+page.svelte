cc6cacf09a7f562b603808810a8a00ea7711e0bc
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 00:11:35 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
 <script>
+	import { page } from "$app/stores";
 	import {
 		Navbar,
 		NavBrand,
 		NavLi,
 		NavUl,
 		NavHamburger,
+		Heading,
 	} from "flowbite-svelte";
+	$: activeUrl = $page.url.pathname;
+	let activeClass =
+		"text-white bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
+	let nonActiveClass =
+		"text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
 </script>
 
 <Navbar let:hidden let:toggle>
@@ -22,10 +29,15 @@
 		</span>
 	</NavBrand>
 	<NavHamburger on:click={toggle} />
-	<NavUl {hidden}>
-		<NavLi href="/home">Home</NavLi>
-		<NavLi href="/banks">Question banks</NavLi> <!-- question papers, decide whether to include answer keys or not -->
-		<NavLi href="/programs">Programs</NavLi> <!-- programs in java, c++, and python -->
+	<NavUl {activeUrl} {activeClass} {nonActiveClass}>
+		<NavLi href="/home" active={true}>Home</NavLi>
+		<NavLi href="/banks">Question banks</NavLi>
+		<!-- question papers, decide whether to include answer keys or not -->
+		<NavLi href="/programs">Programs</NavLi>
+		<!-- programs in java, c++, and python -->
 		<NavLi href="/about">About</NavLi>
 	</NavUl>
 </Navbar>
+<div class="text-center flex justify-self-stretch justify-items-center">
+	<Heading tag="h1" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl text-black dark:text-white">Heading text</Heading>
+</div>
