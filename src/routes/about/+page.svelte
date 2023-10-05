966a4e83b60c0212889ae3f9e60444b8ddedd146
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 23:46:25 +0530

    Update about\+page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,6 @@
 <script>
-	import {
-		Navbar,
-		NavBrand,
-		NavLi,
-		NavHamburger,
-        P,
-	} from "flowbite-svelte";
-    import NavUl from '../../NavUl.svelte';
+	import { Navbar, NavBrand, NavLi, NavHamburger, Heading, P } from "flowbite-svelte";
+	import NavUl from "../../NavUl.svelte";
 </script>
 
 <Navbar let:hidden let:toggle>
@@ -25,11 +19,15 @@
 	<NavHamburger on:click={toggle} />
 	<NavUl>
 		<NavLi href="/home">Home</NavLi>
-		<NavLi href="/banks">Question banks</NavLi> <!-- question papers, decide whether to include answer keys or not -->
-		<NavLi href="/programs">Programs</NavLi> <!-- programs in java, c++, and python -->
+		<NavLi href="/banks">Question banks</NavLi>
+		<NavLi href="/programs">Programs</NavLi>
 		<NavLi href="/about" active={true}>About</NavLi>
 	</NavUl>
 </Navbar>
-<div class="text-center">
-    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 text-gray-400">[insert text here]</P>
+<div class="text-left sm:px-16 xl:px-48">
+	<Heading tag="h2" customSize="pt-6 text-2xl font-medium md:text-xl lg:text-4xl text-black dark:text-white">About</Heading>
+	<P class="mb-6 text-lg lg:text-xl pt-6 dark:text-gray-400"
+		>This website is a hobby project made possible by me, donut, and a few
+		other friends who helped me to gather the content for this website.</P
+	>
 </div>
