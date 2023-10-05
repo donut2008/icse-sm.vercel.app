698162127cd713e53e6a21a28c16806b20ca5668
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-05 23:45:33 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,12 +6,6 @@ const config = {
 	darkMode: 'media',
 
 	theme: {
-		fontFamily: {
-			'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-			'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-			'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-
-		},
 		extend: {
 			fontFamily: {
 				'sans': [
