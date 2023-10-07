fea204b4a8512a96c6eb0b7eaa65b15831648f1c
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-07 23:08:31 +0530

    Update banks\+page.svelte

--- src/routes/banks/+page.svelte
+++ src/routes/banks/+page.svelte
@@ -1,35 +1,16 @@
 <script>
-	import {
-		Navbar,
-		NavBrand,
-		NavLi,
-		NavHamburger,
+	import { Heading,
         P,
 	} from "flowbite-svelte";
-    import NavUl from '../../NavUl.svelte';
 </script>
 
-<Navbar let:hidden let:toggle>
-	<NavBrand href="/">
-		<img
-			src="https://flowbite.com/docs/images/logo.svg"
-			class="mr-3 h-6 sm:h-9"
-			alt="ICSE Study Material Logo"
-		/>
-		<span
-			class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-		>
-			ICSE Study Material
-		</span>
-	</NavBrand>
-	<NavHamburger on:click={toggle} />
-	<NavUl>
-		<NavLi href="/home">Home</NavLi>
-		<NavLi href="/banks" active={true}>Question banks</NavLi>
-		<NavLi href="/programs">Programs</NavLi>
-		<NavLi href="/about">About</NavLi>
-	</NavUl>
-</Navbar>
-<div class="text-center">
-    <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Under construction</P>
+<div class="text-left sm:px-16 xl:px-48">
+	<Heading
+		tag="h2"
+		customSize="pt-6 text-2xl font-medium md:text-xl lg:text-4xl text-black dark:text-white"
+		>Question Banks</Heading
+	>
+	<P class="mb-6 text-lg lg:text-xl pt-6 dark:text-gray-400"
+		>Coming soon...</P
+	>
 </div>
