b7a6b8c1ee36a0c437977b94bc23caa171e26c2b
donut2008 <76439683+donut2008@users.noreply.github.com>
2023-10-07 23:08:50 +0530

    Update home\+page.svelte

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,51 +1,14 @@
 <script>
-	import { page } from "$app/stores";
-	import {
-		Navbar,
-		NavBrand,
-		NavLi,
-		NavUl,
-		NavHamburger,
-		Heading,
-		P,
-	} from "flowbite-svelte";
-	$: activeUrl = $page.url.pathname;
-	let activeClass =
-		"text-white bg-green-700 md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
-	let nonActiveClass =
-		"text-gray-700 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+	import { Heading, P } from "flowbite-svelte";
 </script>
 
-<Navbar let:hidden let:toggle>
-	<NavBrand href="/">
-		<img
-			src="https://flowbite.com/docs/images/logo.svg"
-			class="mr-3 h-6 sm:h-9"
-			alt="ICSE Study Material Logo"
-		/>
-		<span
-			class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-		>
-			ICSE Study Material
-		</span>
-	</NavBrand>
-	<NavHamburger on:click={toggle} />
-	<NavUl {activeUrl} {activeClass} {nonActiveClass}>
-		<NavLi href="/home" active={true}>Home</NavLi>
-		<NavLi href="/banks">Question banks</NavLi>
-		<NavLi href="/programs">Programs</NavLi>
-		<NavLi href="/about">About</NavLi>
-	</NavUl>
-</Navbar>
-<div class="text-center object-center">
+<div class="text-left sm:px-16 xl:px-48">
 	<Heading
-		tag="h1"
-		class="mt-16 mb-16"
-		customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl text-black dark:text-white"
-		>Welcome to ICSE Study Material!</Heading
+		tag="h2"
+		customSize="pt-6 text-2xl font-medium md:text-xl lg:text-4xl text-black dark:text-white"
+		>Home</Heading
 	>
-	<P
-		class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 text-gray-100 dark:text-gray-400"
+	<P class="mb-6 text-lg lg:text-xl pt-6 dark:text-gray-400"
 		>This site is still under construction. Feel free to look around to see
 		what's done (if anything is, so far)!</P
 	>
